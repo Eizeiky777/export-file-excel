688679776c54803ac91395edf4b285b1bcc4d701
putu_crisayoga <putu.crisayoga@meteor.id>
2021-10-23 17:52:49 +0700

    export file excel

--- controller/export-file.js
+++ controller/export-file.js
@@ -168,3 +168,107 @@ exports.exportingFilesExcel = async (req, res) => {
     return res.send(err);
   }
 };
+
+exports.exportingFilesExcelAppend = async (req, res) => {
+  const data = tutorials;
+
+  try {
+    // Buat Workbook
+    const fileName = 'person';
+    const wb = XLSX.utils.book_new();
+    wb.Props = {
+      Title: fileName,
+      Author: 'John Doe',
+      CreatedDate: new Date(),
+    };
+    // Buat Sheet
+    wb.SheetNames.push('Sheet 1');
+    // Buat Sheet dengan Data
+    const ws = XLSX.utils.json_to_sheet(data);
+
+    // Cek apakah folder downloadnya ada
+    const downloadFolder = path.resolve(__dirname, '../downloads');
+
+    if (!fs.existsSync(downloadFolder)) {
+      fs.mkdirSync(downloadFolder);
+    }
+
+    if (fs.existsSync(`${downloadFolder}${path.sep}${fileName}.xlsx`)) {
+      fs.unlinkSync(`${downloadFolder}${path.sep}${fileName}.xlsx`);
+      console.log('delete file');
+    }
+
+    try {
+      //
+      const appendingExcel = [];
+      const totalPage = [1];
+      const appendData = [
+        {
+          id: 999999,
+          name: 'TEST APPENDING DATA',
+          age: 1000,
+        },
+      ];
+
+      totalPage.map(() => {
+        appendingExcel.push(XLSX.utils.sheet_add_json(ws, appendData, { skipHeader: true, origin: -1 }));
+        return true;
+      });
+
+      await Promise.all(appendingExcel);
+
+      // Simpan filenya
+      wb.Sheets['Sheet 1'] = ws;
+      XLSX.writeFile(wb, `${downloadFolder}${path.sep}${fileName}.xlsx`);
+
+      const cloudinary = await startCloudinary();
+      const randUUID = v1();
+
+      const uploadingExcel = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(`${downloadFolder}${path.sep}${fileName}.xlsx`,
+          {
+            resource_type: 'auto',
+            public_id: randUUID,
+          }, (error, result) => {
+            if (error) return reject(error);
+            if (result) return resolve(result);
+            return true;
+          });
+      });
+
+      if (fs.existsSync(`${downloadFolder}${path.sep}${fileName}.xlsx`)) {
+        fs.unlinkSync(`${downloadFolder}${path.sep}${fileName}.xlsx`);
+        console.log('delete file');
+      }
+
+      return res.send(uploadingExcel);
+      // //
+    } catch (e) {
+      console.log(e.message);
+      return res.send(e);
+    }
+
+    // samplet output
+    // {
+    //   "asset_id": "7286e2c45fb9f139aff73e559bc21e44",
+    //   "public_id": "5640b300-33e9-11ec-ba8b-5127589e7759.xlsx",
+    //   "version": 1634983782,
+    //   "version_id": "1ee97c1af8c09d360b767c85e363a3dc",
+    //   "signature": "22848b77d9ff9af7f9214d3f5de8e9fd8fa867cd",
+    //   "resource_type": "raw",
+    //   "created_at": "2021-10-23T10:09:42Z",
+    //   "tags": [],
+    //   "bytes": 22104,
+    //   "type": "upload",
+    //   "etag": "b46b34b9d47d9d2f9ed872a9a1e7b6da",
+    //   "placeholder": false,
+    //   "url": "http://res.cloudinary.com/raw/upload/v1634983782/5640b300-33e9-11ec-ba8b-5127589e7759.xlsx",
+    //   "secure_url": "https://res.cloudinary.com/raw/upload/v1634983782/5640b300-33e9-11ec-ba8b-5127589e7759.xlsx",
+    //   "original_filename": "person",
+    //   "api_key": ""
+    // }
+  } catch (err) {
+    console.log(err);
+    return res.send(err);
+  }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { exportingFilesFastCSV, downloadingFilesJSON2CSV, exportingFilesExcel } = require('../controller/export-file');
+const { exportingFilesFastCSV, downloadingFilesJSON2CSV, exportingFilesExcel, exportingFilesExcelAppend } = require('../controller/export-file');
 
 const router = express.Router();
 
 router.get('/export_file_fastCSV', exportingFilesFastCSV);
 router.get('/export_file_JSON2CSV', downloadingFilesJSON2CSV);
 router.get('/export_file_excel', exportingFilesExcel);
+router.get('/export_file_excel_with_append', exportingFilesExcelAppend);
 
 module.exports = router;
